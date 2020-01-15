96ac5636a8344ed84e4350d9f661ead5a18b8cf6
Michal Kubišta <michal.kubista@nielsen.com>
2020-01-15 14:52:33 +0100

    Removing the filter (not needed)

--- the_amazing_analysis.R
+++ the_amazing_analysis.R
@@ -10,14 +10,10 @@ sales = as.data.table(mtcars)[,.(
 )]
 
 sales[
-    retailer == 2,
     promo_sales := sales / promo
     ][
         retailer %in% c(1,3,4),
         promo := 0
-        ][
-            retailer != 2 & promo == 1,
-            promo_sales := sales * promo
         ]
 
 print(sales[, mean(promo_sales, na.rm = T)])
